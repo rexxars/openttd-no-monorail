e13b9bec7005b45043d9bab2ee31b0b24b5f2804
Espen Hovlandsdal <espen@hovlandsdal.com>
2021-02-07 16:00:30 -0800

    Exclude license from tarball

--- build.sh
+++ build.sh
@@ -5,7 +5,6 @@ rm -rf dist && \
   cd src && \
   nmlc -o ../dist/no-monorail.grf no-monorail.nml && \
   cd .. && \
-  cp LICENSE dist/license.txt && \
   cp README.md dist/readme.txt && \
   cp CHANGELOG.md dist/changelog.txt && \
   tar -cf no-monorail.tar dist  && \
